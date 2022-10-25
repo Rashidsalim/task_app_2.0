c68218c0133c3cc657c3fc34e196009d585cdce8
Rashid Salim <rashid@DESKTOP-B185NVM.localdomain>
2022-10-26 00:17:03 +0300

    Added Numbered List

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ window.addEventListener('load',()=> {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
+    const number_list = document.getElementById("numbered_list");
+
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         const task = input.value;
         const task_el = document.createElement('div');
         task_el.classList.add('task');
-        
-
+        // Form Validation on Null Submit
+        if (task == "" || task == null) {
+            alert("Please Add A Task");
+            return false;
+            }
         const task_content_el = document.createElement('div');
         task_content_el.classList.add('content');
 
@@ -54,6 +59,10 @@ window.addEventListener('load',()=> {
 
         input.value = '';
 
+        for (var i = 0; i < number_list.length; i++) {
+            list_el += "<li>" + number_list[i] + "</li>";
+        }
+        
         list_el.appendChild(task_el);
 
         // number the list
